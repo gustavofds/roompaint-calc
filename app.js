7c105f629a54890d5ffeaec5614cb2132114768a
Gustavo Sousa <gustavohl98@gmail.com>
2021-10-29 22:04:52 -0300

    Implementa middlewares express.json() e .static() e organizacao inicial de pastas

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (_req, res) => {
   res.status(200).send('Test');
