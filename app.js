10c1f0b1f55e0bdb3c5f69436f65f20cd997243d
Gustavo Sousa <gustavohl98@gmail.com>
2021-10-29 23:18:42 -0300

    Cria controller de Erro para devolver erros para o cliente

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const Door = require('./models/Door');
 const Window = require('./models/Window');
 const Wall = require('./models/Wall');
+const errorHandler = require('./controllers/errorController');
 
 const app = express();
 
@@ -10,12 +11,14 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (_req, res) => {
-  const parede1 = new Wall(500, 2, 1, 1);
+  const parede1 = new Wall(2, 2, 1, 1);
   res.status(200).send(`
   Área da porta: ${Door.getArea()} m2\n
   Área da janela: ${Window.getArea()} m2\n
-  Área da parede1: ${parede1.getArea()}m2
+  Área da parede1: ${parede1.getArea()}m2\n
   `);
 });
+
+app.use(errorHandler);
 
 module.exports = app;
