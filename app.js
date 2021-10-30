f6fef537e2d6b9a4dc8f987f44eb59de3f0273ea
Gustavo Sousa <gustavohl98@gmail.com>
2021-10-30 00:22:04 -0300

    Implementa logica para calcular latas de tinta necessarias

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (_req, res) => {
-  const parede1 = new Wall(2.2, 4, 1, 1);
-  const parede2 = new Wall(2.2, 4, 1, 1);
-  const parede3 = new Wall(2.2, 4, 1, 1);
-  const parede4 = new Wall(2.2, 4, 1, 1);
+  const parede1 = new Wall(2.2, 2, 0, 0);
+  const parede2 = new Wall(2.2, 2, 0, 0);
+  const parede3 = new Wall(2.2, 2, 0, 0);
+  const parede4 = new Wall(2.2, 2, 0, 0);
   const room1 = new Room([parede1, parede2, parede3, parede4]);
 
   res.status(200).json({
@@ -22,6 +22,7 @@ app.get('/', (_req, res) => {
     areasJanelas: parede1.getWindowsArea(),
     areaPintavel: parede1.getPaintableArea(),
     areaPintavelQUARTO: room1.getTotalPaintableArea(),
+    titas: room1.calcPaintQty(),
   });
 });
 
--- models/Room.js
+++ models/Room.js
@@ -11,6 +11,25 @@ class Room {
       return prev + curr.getPaintableArea();
     }, 0);
   }
+
+  calcPaintQty() {
+    const totalLitersNeeded = this.getTotalPaintableArea() / 5;
+
+    const gal18 = Math.trunc(totalLitersNeeded / 18);
+    let remainder = totalLitersNeeded % 18;
+    const gal3d6 = Math.trunc(remainder / 3.6);
+    remainder %= 3.6;
+    const gal2d5 = Math.trunc(remainder / 2.5);
+    remainder %= 2.5;
+    const gal0d5 = Math.ceil(remainder / 0.5);
+
+    return {
+      gal18,
+      gal3d6,
+      gal2d5,
+      gal0d5,
+    };
+  }
 }
 
 module.exports = Room;
