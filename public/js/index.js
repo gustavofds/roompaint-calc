7c52571a4c9921bece57280683371846ddd5de74
Gustavo Sousa <gustavohl98@gmail.com>
2021-11-01 19:15:30 -0300

    Implementacao inicial da logica de fazer a requisicao POST

---
+++ public/js/index.js
@@ -0,0 +1,24 @@
+const wallsForm = document.getElementById('walls-form');
+
+wallsForm.addEventListener('submit', (ev) => {
+  ev.preventDefault();
+
+  let index = 0;
+  const wallsData = [];
+  while (document.getElementById(`wall-${index}-data`)) {
+    wallsData.push({
+      height: parseFloat(document.getElementById(`wall-height-${index}`).value),
+      width: parseFloat(document.getElementById(`wall-width-${index}`).value),
+      doorCount: parseFloat(
+        document.getElementById(`doors-count-${index}`).value
+      ),
+      windowCont: parseFloat(
+        document.getElementById(`windows-count-${index}`).value
+      ),
+    });
+
+    index++;
+  }
+
+  console.log(wallsData);
+});
