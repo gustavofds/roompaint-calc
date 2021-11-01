7473bf3460a0ee782532aee05e9a797863e9aea6
Gustavo Sousa <gustavohl98@gmail.com>
2021-11-01 17:30:18 -0300

    Implementacao da rota POST /api/paint finalizada

--- controllers/paintController.js
+++ controllers/paintController.js
@@ -1,6 +1,21 @@
 const Wall = require('../models/Wall');
 const Room = require('../models/Room');
 
-exports.calc = (_req, res) => {
-  res.status(200).send('OK');
+exports.calc = (req, res, next) => {
+  try {
+    const wallsArr = req.body.map(
+      ({ height, width, doorCount, windowCount }) => {
+        return new Wall(height, width, doorCount, windowCount);
+      }
+    );
+
+    const room = new Room(wallsArr);
+
+    res.status(200).json({
+      totalPaintableArea: room.getTotalPaintableArea(),
+      paintNeeded: room.calcPaintQty(),
+    });
+  } catch (err) {
+    next(err);
+  }
 };
